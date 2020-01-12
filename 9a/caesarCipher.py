347aea593a380aac927aea0ad14fca8b4e9dc53c
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 21:04:32 -0500

    Finished 9a.

--- 9a/caesarCipher.py
+++ 9a/caesarCipher.py
@@ -1,7 +1,10 @@
+# List in alphabetical order
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 while True:
     userinput = input()
     shift = int(userinput.split(", ")[1])
     base = userinput.split(", ")[0]
-    for i in range(0, len(base) - 1):
-        print()
+    for i in range(1, (len(base) - 1)):
+        print(alphabet[alphabet.index(base[i]) - shift], end='')
     print()
